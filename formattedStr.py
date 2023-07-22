6cdc6d98612fbf4ed8a763ba9e90f699f11ae1b4
Muhammad SA <MuhammadSA211999>
2023-07-22 10:05:29 +0600

    formatted string and type() func

---
+++ formattedStr.py
@@ -0,0 +1,19 @@
+# formatted string e f"" bebohar kore tar moddhye string format kora zay.
+# ekoi line e print korar jonno end" " bebohar korte hoy. end"" korlen notun line toiri hoy na.
+num1 = 12
+num2 = 13
+
+result = print(f"{num1}+{num2} are make = {num1+num2}")
+
+# end="" bebohar kore ekoi line print kora zay.
+name = 'MSA'
+mNum = '0137464466'
+print(name, end=" ")
+print(mNum)
+
+# built in  type() func bebohar kore zekunu kichur type find out korte hoy.
+nameType = type(name)
+print(nameType)
+
+num1Type = type(num1)
+print(num1Type)
