635cfe83f67ae5ea3e8fd459b5bced61c7fddc18
Muhammad SA <MuhammadSA211999>
2023-07-22 14:51:29 +0600

    break and continue

---
+++ breakeContinue.py
@@ -0,0 +1,23 @@
+'''
+Break and Continue:
+break and continue kuno ekti condition er odhine kaj kore.
+**condtion true hole break loop run bondho kore dey.
+**continue condtion true hole loop sei print na kore, abar loop e firiye dey.
+'''
+
+# i = 1
+# while i <= 100:
+#     if (i == 9):
+#         break
+#     print(i)
+#     i = i+1
+# if (i == 9):
+#     break
+
+
+i = 1
+while i <= 100:
+    print(i)
+    i = i+1
+    if i == 20:
+        continue
